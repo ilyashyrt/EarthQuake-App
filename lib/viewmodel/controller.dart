8e954c476de501199881bdb754be890e9c48f0ee
DESKTOP-PBREBOD\ILYAS <ihayrat786@gmail.com>
2021-11-11 13:24:39 +0300

    Enum kaldırıldı

--- lib/viewmodel/controller.dart
+++ lib/viewmodel/controller.dart
@@ -2,33 +2,31 @@ import 'package:earthquake_app/models/model.dart';
 import 'package:earthquake_app/services/service.dart';
 import 'package:get/get.dart';
 
-enum EarthquakeState{ IDLE, BUSY, ERROR}
+class EarthquakeController extends GetxController {
+  var isLoading = true.obs;
+  var earthquakeListObs = <EarthQuakeData>[].obs;
 
-class EarthquakeController extends GetxController{
- var isLoading = true.obs;
- var earthquakeListObs = <EarthQuakeData>[].obs;
-
- @override
+  @override
   void onInit() {
     fetchData();
     //print(earthquakeListObs[0].result[0].title);
     super.onInit();
   }
 
- void fetchData() async{
-   try{
-     isLoading(true);
-     var earthquakes = await WebService.getData();
-     if(earthquakes != null){
-       //earthquakeListObs.map((e) => e.result.map((e) => e.title));
-       for (int i=0;i<earthquakes.result.length;i++) {
-         earthquakeListObs.add(earthquakes);
-       }//BURADA HATA VAR !!!!!!!!!!!!!!!!
-     }
-   }finally{
-     isLoading(false);
-   }
+  void fetchData() async {
+    try {
+      isLoading(true);
+      var earthquakes = await WebService.getData();
+      if (earthquakes != null) {
+        //earthquakeListObs.map((e) => e.result.map((e) => e.title));
+        for (int i = 0; i < earthquakes.result.length; i++) {
+          earthquakeListObs.add(earthquakes);
+        }
+        //earthquakeListObs.refresh(); //GÜNCELLENEN VERİLERİ EKRANA YANSITMASI İÇİN YAZDIM AMA ÇALIŞMADI!!!!
 
- }
-   
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
 }
